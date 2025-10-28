90d55770ed611fafe242b83733dc6045d7189321
Gabriele Amorosino <g.amorosino@gmail.com>
2025-10-28 17:26:06 -0500

    add script for donwloading a data semple

---
+++ download_set_sample.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
+
+local_dir=$( dirname $SCRIPT_DIR )
+bl_data_hand=${local_dir}/bl_data_hand
+bl_data_download=${bl_data_hand}/bl_data_download.sh
+
+
+if [ ! -f ${bl_data_download}  ]; then
+
+    cd ${local_dir}
+    git clone https://github.com/gamorosino/bl_data_hand.git
+
+    cd ${SCRIPT_DIR}
+
+fi
+
+
+output_dir=$1
+
+if [[ -z "$output_dir" ]]; then
+    echo "Usage: $0 <output_dir> "
+    exit 1
+fi
+
+bash ${bl_data_download}   5ffc884d2ba0fba7a7e89132 \
+                            neuro/freesurfer  \
+                            ${output_dir} \
+                            1 \
+                            --output-prefix datatype,datatype_tag \
+                            --subject 100206
+
+bash ${bl_data_download}   5ffc884d2ba0fba7a7e89132 \
+                            neuro/csd \
+                            ${output_dir} \
+                            1 \
+                            --output-prefix datatype,datatype_tag \
+                            --subject 100206
+
+bash ${bl_data_download}   5ffc884d2ba0fba7a7e89132 \
+                            neuro/tensor  \
+                            ${output_dir} \
+                            1 \
+                            --output-prefix datatype,tag \
+                            --subject 100206 mrtrix3_tensor
+
+bash ${bl_data_download}   5ffc884d2ba0fba7a7e89132 \
+                            neuro/mask \
+                            ${output_dir} \
+                            1 \
+                            --output-prefix datatype,datatype_tag \
+                            --subject 100206 \
+                            '+5tt' '+anat' '+5tt_masks'
